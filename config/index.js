7cf56a0d154761c2733241ab9b5f4d4146806271
Frail Bongat <frail.bongat@gmail.com>
2021-05-23 11:47:09 +0800

    Build: update production server url

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://next-crash-course-mdang3vf4-frailbongat.vercel.app';
+  : 'http://next-crash-course-96wbu70jc-frailbongat.vercel.app';
