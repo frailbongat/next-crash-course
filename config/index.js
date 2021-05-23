1be5b274c9c01835c7b09d9f3355b95b21183a5c
Frail Bongat <frail.bongat@gmail.com>
2021-05-23 11:54:14 +0800

    Build: add temporary production url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev
-  ? 'http://localhost:3000'
-  : 'https://next-crash-course-n5plarajr-frailbongat.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000';
